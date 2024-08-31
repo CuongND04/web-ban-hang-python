3c8a07e544eac6b12fba3bc03d16fef305f45e98
Nguyễn Đức Cường <uteu237@gmail.com>
2024-08-31 15:31:38 +0700

    ✅ thêm ảnh cho sản phẩm

--- app/models.py
+++ app/models.py
@@ -12,8 +12,17 @@ class Product(models.Model):
   name = models.CharField(max_length=200,null=True )
   price = models.FloatField()
   digital = models.BooleanField(default=False,null=True,blank=False)
+  image = models.ImageField(null=True,blank=True)
   def __str__(self):
     return self.name
+  @property
+  def ImageUrl(self):
+    try:
+      url = self.image.url
+    except:
+      url = ""
+    return url
+      
 
 class Order(models.Model):
   customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank = True)
