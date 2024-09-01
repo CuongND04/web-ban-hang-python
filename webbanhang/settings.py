091fb5e85ee21959ea8558464304b6c894a6df50
Nguyễn Đức Cường <uteu237@gmail.com>
2024-09-01 13:52:01 +0700

    🔧 Kết nối với mysql

--- webbanhang/settings.py
+++ webbanhang/settings.py
@@ -76,9 +76,12 @@ WSGI_APPLICATION = 'webbanhang.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "sitesale",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
     }
 }
 
