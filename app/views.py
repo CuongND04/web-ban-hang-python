70087706217e9d29f5a1fe7cf7ce0149e03577ad
Nguyễn Đức Cường <uteu237@gmail.com>
2024-09-01 11:15:42 +0700

    🎨 hiển thị category

--- app/views.py
+++ app/views.py
@@ -6,7 +6,34 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 # Create your views here.
+def category(request):
+  #  giỏ hàng
+  if request.user.is_authenticated:
+  # tắt số lượng trên giỏ hàng
+    customer = request.user
+    order,created = Order.objects.get_or_create(customer=customer,complete = False)
+    items  = order.orderitem_set.all()
+    cartItems = order.get_cart_items
+    user_not_login = "none"
+    user_login = "block"
+  else:
+    # hiển thị số lượng trên giỏ hàng
+    items = []
+    order = {'get_cart_items':0,'get_cart_total': 0}
+    cartItems = order['get_cart_items']
+    user_not_login = "block"
+    user_login = "none"
+  # END  giỏ hàng
+  categories = Category.objects.filter(is_sub=False)
+  active_category = request.GET.get('category','')
+  if active_category:
+    products = Product.objects.filter(category__slug = active_category)
+  context = {'categories':categories,'products':products,'active_category':active_category,'user_not_login':user_not_login,'user_login':user_login,'cartItems':cartItems}
+  return render(request,'app/category.html',context)
+
+
 def search(request):
+  #  giỏ hàng
   if request.user.is_authenticated:
     customer = request.user
     order,created = Order.objects.get_or_create(customer=customer,complete = False)
@@ -21,21 +48,26 @@ def search(request):
     cartItems = order['get_cart_items']
     user_not_login = "block"
     user_login = "none"
+  # END giỏ hàng
   products = Product.objects.all()
+  categories = Category.objects.filter(is_sub=False)
   if request.method == "POST":
     searched = request.POST["searched"]
     keys = Product.objects.filter(name__contains = searched)
-  context = {"searched":searched,"keys":keys,'products':products,'cartItems':cartItems,'user_not_login':user_not_login,'user_login':user_login }
+  context = {"searched":searched,"keys":keys,'products':products,'cartItems':cartItems,'user_not_login':user_not_login,'user_login':user_login,'categories':categories }
   return render(request,'app/search.html',context)
 
 def register(request):
+  categories = Category.objects.filter(is_sub=False)
+  user_not_login = "block"
+  user_login = "none"
   form = CreateUserForm()
   if request.method == "POST":
     form = CreateUserForm(request.POST)
     if form.is_valid():
       form.save()
       return redirect('login')
-  context={'form':form}
+  context={'form':form,'user_not_login':user_not_login,'user_login':user_login ,'categories':categories }
   return render(request,'app/register.html',context)
 
 
@@ -45,6 +77,7 @@ def loginPage(request):
     return redirect('home')
   user_not_login = "block"
   user_login = "none"
+  categories = Category.objects.filter(is_sub=False)
   if request.method == "POST":
     username = request.POST.get('username')
     password = request.POST.get('password')
@@ -53,7 +86,7 @@ def loginPage(request):
       login(request,user)
       return redirect('home')
     else: messages.info(request,'user or password not correct!')
-  context={'user_not_login':user_not_login,'user_login':user_login }
+  context={'user_not_login':user_not_login,'user_login':user_login,'categories':categories }
   return render(request,'app/login.html',context)
 
 
@@ -77,8 +110,9 @@ def home(request):
     cartItems = order['get_cart_items']
     user_not_login = "block"
     user_login = "none"
+  categories = Category.objects.filter(is_sub=False)
   products = Product.objects.all()
-  context={'products':products,'cartItems':cartItems,'user_not_login':user_not_login,'user_login':user_login }
+  context={'categories':categories,'products':products,'cartItems':cartItems,'user_not_login':user_not_login,'user_login':user_login }
   return render(request,'app/home.html',context)
 
 
@@ -97,8 +131,8 @@ def cart(request):
     cartItems = order['get_cart_items']
     user_not_login = "block"
     user_login = "none"
-
-  context={'items':items,'order':order,'cartItems':cartItems,'user_not_login':user_not_login,'user_login':user_login }
+  categories = Category.objects.filter(is_sub=False)
+  context={'items':items,'order':order,'cartItems':cartItems,'user_not_login':user_not_login,'user_login':user_login,'categories':categories }
   return render(request,'app/cart.html',context)
 
 def checkout(request):
